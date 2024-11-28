ec8e9754a8c8755b01584929de57a5f209b4b72d
feffe1702 <fpm.asd@gmail.com>
2024-11-28 21:42:34 +0100

    [WIP] import some flags

--- src/Utility/getFlag.js
+++ src/Utility/getFlag.js
@@ -1,6 +1,6 @@
 export const getFlag = (languageCode) => {
     try {
-        return new URL(`/src/assets/flags/${languageCode}.png`, import.meta.url).href;
+        return new URL(`/src/assets/flags/${languageCode}.svg`, import.meta.url).href;
     } catch {
         return new URL('/src/assets/flags/default.png', import.meta.url).href;
     }
