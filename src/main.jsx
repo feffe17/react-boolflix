521b4c8c087c9d6ceaaed16b903eef261e408005
feffe1702 <fpm.asd@gmail.com>
2024-11-28 15:26:57 +0100

    [FIX]

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { MovieProvider } from './store/MovieContext';
+import { MovieProvider } from "./Context/MovieContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <MovieProvider>
