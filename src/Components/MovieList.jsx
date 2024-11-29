246c5d8214d0466d6c8358a4fb8ce40d0a9e51ec
feffe1702 <fpm.asd@gmail.com>
2024-11-29 18:15:22 +0100

    [WIP] add layer to movies

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -15,13 +15,24 @@ const MovieList = () => {
         <div className='row'>
             {movies.map((movie) => (
                 <div
-                    className='col-lg-4 col-md-6 col-12 mt-3 mb-3'
+                    className='col-lg-4 col-md-6 col-12 mt-3 mb-5'
                     ky={movie.id}>
-                    <img
-                        className='w-100'
-                        src={getPosterUrl(movie.poster_path)}
-                        alt={movie.title}
-                    />
+                    <div
+                        className={style.ImgBox}
+                    >
+                        <img
+                            className={style.img}
+                            src={getPosterUrl(movie.poster_path)}
+                            alt={movie.title}
+                        />
+                        <div className={style.layer}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#ffffff" class="" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                            </svg>
+                        </div>
+
+                    </div>
                     <div className="bg-dark text-light p-2 mt-2 rounded">
 
                         <h2>{movie.title}</h2>
